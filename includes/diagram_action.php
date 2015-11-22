6c3ea98a04d94d7def6750ca2b544ae0f5512753
desistonv <desistonv@g.cofc.edu>
2015-11-22 11:22:19 -0500

    couple more fixes

--- includes/diagram_action.php
+++ includes/diagram_action.php
@@ -5,8 +5,8 @@ echo
 <div class="box">
    <a class="close" id="closer" href="#">X</a>
  
-<div class="content">
-       <img src="includes/er_diagram.png">
+<div class="content" style="margin-bottom : -40px;">
+       <img class="picture" style="margin-bottom : -40px;" src="includes/er_diagram.png?v=1.1">
  
  
  
