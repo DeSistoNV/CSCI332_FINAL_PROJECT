0d96aada74640006fded3f05b1ca64653144b42f
desistonv <desistonv@g.cofc.edu>
2015-11-22 00:10:03 -0500

    moved a css file

--- includes/head.php
+++ includes/head.php
@@ -9,13 +9,14 @@ echo
 	<link rel="stylesheet" href="style/input_style.css?v=1.1">
 	<link rel="stylesheet" href="style/table_st.css?v=1.1">
    <link rel="stylesheet" href="style/lightbox.css?v=1.1.4">
+   <link rel="stylesheet" href="../style/pop.min.css?v=1.1.1" >
+
    <script src="js/jquery-1.11.3.min.js"></script>
 
    <script src="js/sorttable.js"></script>
    <script src="js/lengthcheck.js"></script>
    <script src="js/notify.js"></script>
 
-   <link href="js/pop.min.css?v=1.4" rel="stylesheet">
    <script src="js/pop.min.js"></script>
 
 
--- includes/save_head.php
+++ includes/save_head.php
@@ -7,13 +7,14 @@ echo
 	<link rel="stylesheet" href="../style/styles.css?v=1.1">
 	<link rel="stylesheet" href="../style/input_style.css?v=1.1">
 	<link rel="stylesheet" href="../style/table_st.css?v=1.1">
+   <rel="stylesheet" link href="../style/pop.min.css" >
+
    <script src="../js/jquery-1.11.3.min.js"></script>
 
    <script src="../js/sorttable.js"></script>
    <script src="../js/lengthcheck.js"></script>
    <script src="../js/notify.js"></script>
 
-   <link href="../js/pop.min.css" rel="stylesheet">
    <script src="../js/pop.min.js"></script>
 
 
